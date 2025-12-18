c2c3024a904d7a243c9bf9f98197a0ced71b8071
kjac <kja@umbraco.dk>
2025-12-18 16:55:40 +0100

    Initial setup

---
+++ src/DependencyInjection/SiteComposer.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Search.Core.DependencyInjection;
+using Umbraco.Cms.Search.Provider.Examine.DependencyInjection;
+
+namespace Site.DependencyInjection;
+
+public sealed class SiteComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder
+            // add core services for search abstractions
+            .AddSearchCore()
+            // add the Examine search provider
+            .AddExamineSearchProvider();
+
+        // force rebuild indexes after startup (awaiting a better solution from Core)
+        builder.RebuildIndexesAfterStartup();
+    }
+}
